8db1a23ff671862888fa47c8a30b6d175e42c5de
lokeshmi077 <lokeshmishra88588@gmail.com>
2025-06-16 17:53:42 +0530

    api

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\Api\LoginController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::middleware('auth:sanctum')->get('/user', [LoginController::class, 'user']);
